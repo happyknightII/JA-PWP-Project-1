f50861e231d566356982b5418c7a4f0dae715db8
Brian Lee <happyknightII@users.noreply.github.com>
2021-10-05 11:42:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Robot import Robot
 
-#IO.setwarnings(False)
-robot = Robot((14, 15, 24), (17, 27, 23))
-robot.drive((50, 45), 5.0544)
+if __name__ = "__main__":
+    #IO.setwarnings(False)
+    robot = Robot((14, 15, 24), (17, 27, 23))
+    robot.drive((50, 45), 5.0544)

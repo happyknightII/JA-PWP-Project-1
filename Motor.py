ba3e093b73aa6788e93c8bbbe3ce1f5a18df9c00
pl247231 <70585668+pl247231@users.noreply.github.com>
2021-10-28 19:18:22 -0400

    Update file

--- Motor.py
+++ Motor.py
@@ -1,37 +1,40 @@
 import RPi.GPIO as IO
+
 IO.setmode(IO.BCM)
+
 
 class Motor:
   def __init__(self, pins, ena):
-    IO.setup(pins[0],IO.OUT)
-    IO.setup(pins[1],IO.OUT)
-    IO.setup(ena,IO.OUT)
-    self.pins = (IO.PWM(pins[0],100), IO.PWM(pins[1],100))
-    self.pins[0].start(0)
-    self.pins[1].start(0)
+    IO.setup(pins[0], IO.OUT)
+    IO.setup(pins[1], IO.OUT)
+    IO.setup(ena, IO.OUT)
+    self.pins = (IO.PWM(pins[0], 100), IO.PWM(pins[1], 100))
     self.ena = ena
-  
-  def enable(self, enable = True):
+    
+
+  def enable(self, enable=True):
     if enable:
       IO.output(self.ena, IO.HIGH)
+      self.pins[0].start(0)
+      self.pins[1].start(0)
     else:
       IO.output(self.ena, IO.LOW)
+      self.pins[0].stop()
+      self.pins[1].stop()
 
-  def setPower(self, power, brake = False):
+  def setPower(self, power, brake=False):
     if power > 0:
-      self.enable()
       self.pins[0].ChangeDutyCycle(power)
       self.pins[1].ChangeDutyCycle(0)
     else:
-      if not power:
-        if brake:
-          self.enable()
-          self.pins[0].ChangeDutyCycle(100)
-          self.pins[1].ChangeDutyCycle(100)
-        else:
-          self.enable(False)
-          self.pins[0].ChangeDutyCycle(0)
-          self.pins[1].ChangeDutyCycle(0)
+      if power == 0:
+          if brake:
+            self.pins[0].ChangeDutyCycle(100)
+            self.pins[1].ChangeDutyCycle(100)
+          else:
+            self.enable(False)
+            self.pins[0].ChangeDutyCycle(0)
+            self.pins[1].ChangeDutyCycle(0)
       else:
         self.pins[0].ChangeDutyCycle(0)
         self.pins[1].ChangeDutyCycle(-power)

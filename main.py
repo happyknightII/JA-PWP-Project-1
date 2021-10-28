e6c1fdc6b5e05265441f356ac597bd822d27a1e4
pl247231 <70585668+pl247231@users.noreply.github.com>
2021-10-28 19:18:06 -0400

    Update file

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ from Robot import Robot
 
 #IO.setwarnings(False)
 robot = Robot()
-robot.drive((50, 45), 5.0544)
+x = 0.7
+y = -1
+robot.drive((74.46+x, 68.2+y), 2.9)
+robot.pause(1)
+robot.drive((50+x, 0), 1.35)
+robot.pause(1)
+robot.drive((74.46+x, 68.2+y), 3.5)
+robot.pause(1)
+robot.drive((-66, -68.2), 1.3)
+robot.pause(1)
+robot.drive((50+x, 0), 1.35)
+robot.pause(1)
+robot.drive((74.46+x, 68.2+y), 3)

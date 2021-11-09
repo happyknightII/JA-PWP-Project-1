94007394e729ff1ceec65f0e82cc8ef38a715c00
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2021-11-08 22:09:23 -0500

    Code Cleanup

--- Host.py
+++ Host.py
@@ -3,34 +3,40 @@ from Move.Forward import forward
 from Move.Turn import turn
 from Move.Maze import maze
 from Robot import Robot
+
 app = Flask(__name__)
 robot = Robot()
+
+
 @app.route('/')
 def myapp():
-	message = "To use this app: %s/add?command=___&value=___" % request.base_url
-	return message
+  message = "To use this app: %s/add?command=___&value=___" % request.base_url
+  return message
+
 
 @app.route('/add')
 def add():
-	# Checking that both parameters have been supplied
-	if 'command' in request.args:
-		if not 'value' in request.args:
-			return "value parameter is missing"
-	else:
-		return "2"
+  # Checking that both parameters have been supplied
+  if 'command' in request.args:
+    if not 'value' in request.args:
+      return "value parameter is missing"
+  else:
+    return "2"
 
-	# Make sure they are numbers too
-	command = str(request.args['command'])
-	try:
-		value = float(request.args['time'])
-	except:
-		return "value parameter should be a number"
-	if command =="Maze":
-		maze(Robot)
-	elif command == "Forward":
-		forward(robot, value)
-	elif command == "Turn":
-		turn(robot, value)
-	else:
-		return "Invalid Command: Try using 'Maze', 'Forward', or 'Turn'"
-	return command + " " + str(value)
+  # Make sure they are numbers too
+  command = str(request.args['command'])
+  
+  if command == "Maze":
+    maze(Robot)
+  else:
+    try:
+      value = float(request.args['value'])
+    except:
+      return "value parameter should be a number"
+    if command == "Forward":
+      forward(robot, value)
+    elif command == "Turn":
+      turn(robot, value)
+    else:
+      return "Invalid Command: Try using 'Maze', 'Forward', or 'Turn'"
+  return command + " " + str(value)

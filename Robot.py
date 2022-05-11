b03205291614e201a1cec88630ce7d6a66d9762b
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-05-11 11:21:10 -0400

    Lights

--- Robot.py
+++ Robot.py
@@ -56,9 +56,9 @@ class Robot:
     def indicate(self, index):
         for i in range(72):
             if i < index:
-                self.strip.setPixel(index, (int(i/index), int(i/index), int(i/index)))
+                self.strip.setPixel(index, (int(255 * i/index), int(255 * i/index), int(255 * i/index)))
             else:
-                self.strip.setPixel(index, (int((index-i)/index), int((index-i)/index), int((index-i)/index)))
+                self.strip.setPixel(index, (int(255 * (72-i)/index), int(255 * (72-i)/index), int(255 * (72-i)/index)))
         self.strip.show()
 
     def pause(self, time1):

ac2112f573897e4d00bba9beb6541dc78af634bf
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-05-23 10:56:08 -0400

    Change offset

--- main.py
+++ main.py
@@ -121,12 +121,12 @@ def annotation():
 
                     if controlMode:
                         if leftX < img.shape[1] / 2 and rightX < img.shape[1] / 2:
-                            error = center + settings["offsetPixels"]
+                            error = center - img.shape[1] * 0.1 + settings["offsetPixels"]
                             cv2.circle(img, (int(img.shape[1] / 0.1), 100), 100, (255, 255, 255), -1)
                             print("left turn")
                         elif leftX > img.shape[1] / 2 and rightX > img.shape[1] / 2:
                             cv2.circle(img, (int(img.shape[1] / 0.9), 100), 100, (255, 255, 255), -1)
-                            error = center - img.shape[1] + settings["offsetPixels"]
+                            error = center - img.shape[1] * 0.9 + settings["offsetPixels"]
                             print("right turn")
                         else:
                             error = center - img.shape[1] / 2 + settings["offsetPixels"]
